f026ba0994dd49692e8d3fbe66291e507678a38e
mohammedsjnoube <76224485+mohammedsjnoube@users.noreply.github.com>
2020-12-25 13:55:58 +0200

    Update config.js

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 module.exports = {
-    'TelegramUrl': 'FULL_URL_TO_YOUR_TELEGRAM_BOT_APP', // like https://mybot.herokuapp.net
-    'TelegramToken': 'TELEGRAM_BOT_TOKEN', // Telegram bot token that you should obtain from @botFather in Telegram
-    'ByteScoutAPIKEY': "YOUR_API_KEY_FOR_CLOUDAPI", // bytescout cloud api token, get one at https://bytescout.com/products/developer/cloudapi/index.html
-    'WelcomeMessage': "Welcome to ByteScout!",
-    'UnknownCommand': "Sorry, unknown command. Enter (or click) /start to see all options",
-    'SuccessMessage': "Succesfully processed. Enter (or click) /start to see all options", 
-    'ErrorResponse': "Oops, something went wrong. Please try again or type (or click) /start for help",
-    'ErrorInput': "The input is not supported, please try to change it or contact us at https://bytescout.zendesk.com/hc/en-us/requests/new or mailto:support@bytescout.com",
-    'HTMLtoPDFHeaderText': "This file was generated using <a href='https://telegram.me/bytescoutbot'>@bytescoutbot</a>"
+    'TelegramUrl': 'https://janjerbar42.herokuapp.net', // like https://mybot.herokuapp.net
+    'TelegramToken': '1492781126:AAHjR5fxt_69yc0aWUmhM4TC3MZnSq0tKww', // Telegram bot token that you should obtain from @botFather in Telegram
+    'ByteScoutAPIKEY': "soryasady03@gmail.com_d6edc38670e2a00d", // bytescout cloud api token, get one at https://bytescout.com/products/developer/cloudapi/index.html
+    'WelcomeMessage': "أهلييين فيك بمصنع الورق 🙈 انا ابن هاد البوت @Sy404_bot وهاد المطور الغبي لي صنعني 🙂 @Mr00Lucifer بحبكم 🥺❤️ ماترشوني رش طلبات🥺🥺!",
+    'UnknownCommand': "امر غير معروف ياعمو 🙂. ضغاط  /start لتشوف كل الخيارات لي متاحة عندك🙂 لماشي رح اشتكي عليك ع فرع الجرائم الإلكترونية🙂🙂",
+    'SuccessMessage': "تكرااام تم. ضغاط  /start لتشوف كل الخيارات", 
+    'ErrorResponse': "خطأاا شو عملت فيني 🥺 ضغاط /start لتقدر تفهم شغلي أكتر🙈",
+    'ErrorInput': "لي دخلتووو غير مفهوم إذا عند مشككلة مافهمتها او بدك حل يساعدك المطور مباشرة 🙂 حكيه من هون https://www.facebook.com/mohammedsjnoube أو TG: @Mr00lucifer ",
+    'HTMLtoPDFHeaderText': "هذا الملف تم توليده باستخدام بوت التيليجرام <a href='@Paperfa404_bot'>facebook.com/mohammedsjnoube</a>"
 }
